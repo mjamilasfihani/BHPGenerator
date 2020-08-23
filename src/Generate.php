b87da112c7ca110112f6e89feaea8bcb14342bb2
Mohammad Jamil Asfihani <51300528+mjamilasfihani@users.noreply.github.com>
2020-08-23 16:36:31 +0700

    Update Generate.php

--- src/Generate.php
+++ src/Generate.php
@@ -1,2 +1,20 @@
 <?php
 namespace BHPGenerator;
+
+class Generate
+{
+  
+  /**
+   *
+   */
+  const BHP_VERSION = '1.0';
+  
+  /**
+   *
+   */
+  public function __construct()
+  {
+    return true;
+  }
+
+}
