27a998f400d5cee81b374d2cefd94f3bc40c1f02
Mohammad Jamil Asfihani <51300528+mjamilasfihani@users.noreply.github.com>
2020-08-25 14:05:06 +0700

    Update Generate.php

--- src/Generate.php
+++ src/Generate.php
@@ -1,15 +1,16 @@
-<?php
-namespace BHPGenerator;
+<?php namespace BHPGenerator;
 
 class Generate
 {
   
   /**
+   * echo Generate::BHP_VERSION;
    *
    */
   const BHP_VERSION = '1.0';
 
   /**
+   * echo Generate::default($name, $data, $optional);
    *
    */
   public static function default(string $name = '', array $data = [], array $optional = [])
